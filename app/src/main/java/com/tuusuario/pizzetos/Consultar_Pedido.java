709db6c0de2863e84ee879f5fe9473619d433c98
jehu-dlc <jehugarrix@gmail.com>
2025-01-10 23:54:15 -0600

    Ya quedo Interfaz "Realizar pedido, Consultar Pedido y cambie otras cosillas"

--- app/src/main/java/com/tuusuario/pizzetos/Consultar_Pedido.java
+++ app/src/main/java/com/tuusuario/pizzetos/Consultar_Pedido.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Realizar_pedido extends AppCompatActivity {
+public class Consultar_Pedido extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_realizar_pedido);
+        setContentView(R.layout.activity_consultar_pedido);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
