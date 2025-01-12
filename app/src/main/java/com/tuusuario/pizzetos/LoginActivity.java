ad47acc78c28cadf58067df8f507fe1c9b20eb1c
Tu Nombre <tu_email@ejemplo.com>
2025-01-12 13:37:39 -0600

    Login Funcionando verificar autenticacion

--- app/src/main/java/com/tuusuario/pizzetos/LoginActivity.java
+++ app/src/main/java/com/tuusuario/pizzetos/LoginActivity.java
@@ -1,30 +1,96 @@
 package com.tuusuario.pizzetos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    Button login;
-    EditText email, password;
-
-
+    Button btn_login;
+    EditText correo, contrasena;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        this.setTitle("Login");
+
+        // Inicializar Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Asignar los elementos del layout a las variables
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+        btn_login = findViewById(R.id.btn_login);
+
+        // Configurar el listener para el botón
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailUser = correo.getText().toString().trim();
+                String passUser = contrasena.getText().toString().trim();
+
+                if (emailUser.isEmpty() || passUser.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Ingresar los datos", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser(emailUser, passUser);
+                }
+            }
         });
+    }
+
+    private void loginUser(String emailUser, String passUser) {
+        mAuth.signInWithEmailAndPassword(emailUser, passUser)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("LoginActivity", "Inicio de sesión exitoso");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null) {
+                                Log.d("LoginActivity", "Usuario autenticado: " + user.getEmail());
+                            }
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                            Toast.makeText(LoginActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e("LoginActivity", "Error en el inicio de sesión: " + task.getException());
+                            Toast.makeText(LoginActivity.this, "Error en el inicio de sesión", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("LoginActivity", "Error al iniciar sesión: " + e.getMessage());
+                        Toast.makeText(LoginActivity.this, "Error al iniciar sesión: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            Log.d("LoginActivity", "Usuario autenticado: " + user.getEmail());
+            startActivity(new Intent(LoginActivity.this, Home.class));
+            finish();
+        } else {
+            Log.d("LoginActivity", "No hay un usuario autenticado actualmente.");
+        }
     }
 }
